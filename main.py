b073fcb6477668e9deb1379693561dafbd40dc8a
Buxtak <buxtak@gmail.com>
2026-02-13 22:29:07 -0500

    A: creation of initial files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Asteroids project from Boot.dev - Using WSL and ZED
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+
+
+if __name__ == "__main__":
+    main()

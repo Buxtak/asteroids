9a61178c913e76a10a2d24d92e244c6a2d1cd59c
Buxtak <buxtak@gmail.com>
2026-02-16 06:29:40 -0500

    B: Constants created and imported in main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 
+from constants import *
+
 # Asteroids project from Boot.dev - Using WSL and ZED
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"""Starting Asteroids with pygame version: {pygame.version.ver}")
+        Screen width: {SCREEN_WIDTH}
+        Screen height: {SCREEN_HEIGHT}""")
 
 
 if __name__ == "__main__":

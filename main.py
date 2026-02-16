0aa2164f0cb9aa9de01ebce76d6a7e749b7fd83c
Buxtak <buxtak@gmail.com>
2026-02-16 07:18:54 -0500

    D: Running game, black screen until closed on terminal

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
 
 from constants import *
+from logger import log_state
 
 # Asteroids project from Boot.dev - Using WSL and ZED
 
 
 def main():
+    pygame.init()
     print(f"""Starting Asteroids with pygame version: {pygame.version.ver}")
         Screen width: {SCREEN_WIDTH}
         Screen height: {SCREEN_HEIGHT}""")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
